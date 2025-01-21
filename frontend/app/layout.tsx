a78ebcfc7b0ea0528ecebe86cc394121e4f6451a
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2025-01-20 21:32:43 -0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <ThemeProvider>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antiliased`}
       >
         {children}
       </body>
