b2e7344422ad3da8d3ab803e5f132692cf2d4131
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2025-02-07 19:11:39 -0500

    add create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE anime (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    genre VARCHAR(255),
+    description TEXT,
+    image_url VARCHAR(500),
+    is_verified BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE reviews (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    anime_id INT REFERENCES animes(id) ON DELETE CASCADE,
+    rating INT CHECK (rating BETWEEN 1 AND 10),
+    comment TEXT,
+    PRIMARY KEY (user_id, anime_id)
+);
+
+CREATE TABLE user_anime_status (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    anime_id INT REFERENCES animes(id) ON DELETE CASCADE,
+    status INT CHECK (status BETWEEN 1 AND 3),
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, anime_id)
+);
+
+CREATE TABLE friendship (
+    user_id1 INT REFERENCES users(id) ON DELETE CASCADE,
+    user_id2 INT REFERENCES users(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id1, user_id2),
+    CHECK (user_id1 <> user_id2)
+)
+
+
+
+
